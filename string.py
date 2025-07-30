d6697a71aaaac95f64722e3c77dae0355ff7de63
Sravani T <sravanit@Sravanis-MacBook-Air.local>
2025-07-30 21:25:10 +0530

    pavan

--- string.py
+++ string.py
@@ -1,2 +1,30 @@
 name='sravani'
 print(name)
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
+text = "sravani"
+print(text[0])
+print(text[2])
+print(text[-1])
+print(text[-2])
+
+
+text = "  Hello Python World  "
+my=text.replace("Python","sravani")
+print(text.lower())
+print(text.upper())
+print(text.split())
+print(my)
+print(text.strip())
+
+
+sentence = "learning python is super fun"
+print(sentence)
+print(sentence.title())
+print(sentence[0:8])
+print(sentence[::-1])
+#print(sentence[reversed=True])
+print(sentence.replace("super","really"))
+
