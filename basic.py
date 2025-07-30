c6fc228dfe67ce8225e4030b737f3cfca0f82f81
Sravani T <sravanit@Sravanis-MacBook-Air.local>
2025-07-30 22:15:42 +0530

    chinni

---
+++ basic.py
@@ -0,0 +1,31 @@
+number=6
+print(number)
+
+float=7.9
+print(float)
+
+name="sravani"
+print(name)
+
+number=[1,2,3,4,5]
+print(number)
+
+number=(1,2,3,4,5)
+print(number)
+
+number={1,2,3,4,5}
+print(number)
+
+dict={
+    "name":"sravani",
+    "age":19
+}
+print(dict)
+
+
+name=input("Enter the name: ")
+print(name)
+
+name = "sravani"
+age = 19
+print(f"My name is {name} and I am {age} years old.")
